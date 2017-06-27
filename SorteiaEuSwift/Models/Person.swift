83e3980fa7800ecb3d626a772dcbdcacb8629853
yabenatti <min.benatti@gmail.com>
2017-06-27 10:25:26 -0300

    Creates models

---
+++ SorteiaEuSwift/Models/Person.swift
@@ -0,0 +1,25 @@
+//
+//  Person.swift
+//  SorteiaEuSwift
+//
+//  Created by Yasmin Benatti on 14/06/17.
+//  Copyright © 2017 Yasmin Benatti. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+
+    var twitterId : Int = 0
+    
+    var name : String?
+    var picture : String?
+    
+    
+    init(twitterId: Int, name: String?, picture : String?) {
+        self.twitterId = twitterId
+        
+        self.name = name
+        self.picture = picture
+    }
+}
