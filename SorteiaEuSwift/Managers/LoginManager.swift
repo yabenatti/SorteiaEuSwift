a8ef797a7dd6cf5954948348bc23dbe7643371d2
yabenatti <min.benatti@gmail.com>
2017-07-11 13:19:01 -0300

    New managers

---
+++ SorteiaEuSwift/Managers/LoginManager.swift
@@ -0,0 +1,13 @@
+//
+//  LoginManager.swift
+//  SorteiaEuSwift
+//
+//  Created by Yasmin Benatti on 2017-06-15.
+//  Copyright © 2017 Yasmin Benatti. All rights reserved.
+//
+
+import UIKit
+
+class LoginManager: NetworkManager {
+
+}
---
+++ SorteiaEuSwift/Managers/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  SorteiaEuSwift
+//
+//  Created by Yasmin Benatti on 2017-06-15.
+//  Copyright © 2017 Yasmin Benatti. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
